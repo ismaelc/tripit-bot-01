f942849de6967a45173eebab86a1f663ca3349e0
ismaelc <chrisi@13mbp-11334.gateway.pace.com>
2017-02-07 08:30:32 -0800

    test

--- messages/index.js
+++ messages/index.js
@@ -97,7 +97,7 @@ bot.dialog('/', function(session) {
                         for (var i = 0, len = trips.length; i < len; i++) {
                             var card = new builder.ThumbnailCard(session)
                                 .title('Trip name: ' + trips[i].display_name)
-                                .subtitle(trips[i].start_date) //trips[i].start_date + ' - ' + trips[i].primary_location)
+                                .subtitle(trips[i].start_date + ' - ' + trips[i].primary_location) //trips[i].start_date + ' - ' + trips[i].primary_location)
                                 .text('Your trip to ') //+ trips[i].primary_location + ' from ' + trips[i].start_date + ' to ' + trips[i].end_date)
                                 .images([
                                     builder.CardImage.create(session, trips[i].image_url) //trips[i].image_url)
