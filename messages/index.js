d51fdcbdd24271da91ce51b73f39a114d1edd15c
ismaelc <chrisi@13mbp-11334.gateway.pace.com>
2017-02-07 23:08:04 -0800

    test

--- messages/index.js
+++ messages/index.js
@@ -115,7 +115,9 @@ bot.dialog('/', function(session) {
                                     .buttons([
                                         builder.CardAction.openUrl(session, 'https://www.tripit.com/trip/show/id/' + trips[i].id, 'View in TripIt'),
                                         //builder.CardAction.openUrl(session, 'https://www.tripit.com/trip/show/id/' + trips[i].id, 'Share Trip')
-                                        builder.CardAction.dialogAction(session, "Share", "<trip data to share>", "Share trip")
+                                        builder.CardAction.dialogAction(session, "Share", "<trip data to share>", "Share trip"),
+                                        builder.CardAction.imBack(session, "Up", "up")
+
                                     ]);
                                 cards.push(card);
                             }
